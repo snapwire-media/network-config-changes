0be05c0458c8f22b73310aa6d2349e14723cdae3
marco <marsucsb@gmail.com>
2015-06-19 21:07:14 -0700

    Reduce boilerplate.

--- app/src/main/kotlin/re/snapwi/orientation/event/JokeEvent.kt
+++ app/src/main/kotlin/re/snapwi/orientation/event/JokeEvent.kt
@@ -3,17 +3,6 @@ package re.snapwi.orientation.event
 import re.snapwi.orientation.io.Joke
 import retrofit.RetrofitError
 
-public class JokeEvent {
-  var retrofitError: RetrofitError? = null
-  var joke: Joke? = null
-
-  public constructor(joke: Joke) {
-    this.joke = joke
-  }
-
-  public constructor(retrofitError: RetrofitError) {
-    this.retrofitError = retrofitError
-  }
-
-  public fun hasError(): Boolean = retrofitError != null
+public class JokeEvent(val joke: Joke?, val error: RetrofitError?) {
+  public fun hasError(): Boolean = error != null
 }
